7f86f7fcbd40ddc420f1ff6a91c0253752a9dac1
Controlled Chaos Design <greg@ccdzine.com>
2026-06-17 12:42:01 -0700

    Merge branch 'main' of https://github.com/ControlledChaos/applied-content-forms

--- includes/walkers/class-acf-walker-nav-menu-edit.php
+++ includes/walkers/class-acf-walker-nav-menu-edit.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * CMS functions
  *
@@ -18,7 +18,7 @@ class ACF_Walker_Nav_Menu_Edit extends Walker_Nav_Menu_Edit {
     /**
      * Start the element output
      *
-     * Calls the Walker_Nav_Menu_Edit start_el function and then injects the custom field HTML  
+     * Calls the Walker_Nav_Menu_Edit start_el function and then injects the custom field HTML
      *
 	 * @since  1.0.0
 	 * @access public
@@ -31,12 +31,12 @@ class ACF_Walker_Nav_Menu_Edit extends Walker_Nav_Menu_Edit {
 	 * @return void
      */
 	public function start_el( &$output, $item, $depth = 0, $args = [], $id = 0 ) {
-		
+
 		$item_output = '';
-		
+
 		// Call parent function.
 		parent :: start_el( $item_output, $item, $depth, $args, $id );
-		
+
 		// Inject custom field HTML.
 		$output .= preg_replace(
 			// NOTE: Check this regex from time to time!
@@ -58,9 +58,9 @@ class ACF_Walker_Nav_Menu_Edit extends Walker_Nav_Menu_Edit {
 	 * @return string
 	 */
 	public function get_fields( $item, $depth, $args = [], $id = 0 ) {
-		
+
 		ob_start();
-		
+
 		/**
          * Get menu item custom fields from plugins/themes
          *
@@ -72,5 +72,5 @@ class ACF_Walker_Nav_Menu_Edit extends Walker_Nav_Menu_Edit {
          */
 		do_action( 'wp_nav_menu_item_custom_fields', $item->ID, $item, $depth, $args, $id );
 		return ob_get_clean();
-	}		
+	}
 }
