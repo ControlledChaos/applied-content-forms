92993e8cf9b706ad9c2181315dea7b7ff22d6820
Controlled Chaos Design <greg@ccdzine.com>
2026-07-07 08:50:18 -0700

    Fix (?) call to function on null.

--- includes/locations/class-acf-location-page-template.php
+++ includes/locations/class-acf-location-page-template.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 
 if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 if( ! class_exists('ACF_Location_Page_Template') ) :
 
 class ACF_Location_Page_Template extends ACF_Location {
-	
+
 	/**
 	 * Initializes props.
 	 *
@@ -22,7 +22,7 @@ class ACF_Location_Page_Template extends ACF_Location {
     	$this->object_type = 'post';
     	$this->object_subtype = 'page';
 	}
-	
+
 	/**
 	 * Matches the provided rule against the screen args returning a bool result.
 	 *
@@ -35,26 +35,46 @@ class ACF_Location_Page_Template extends ACF_Location {
 	 * @return	bool
 	 */
 	public function match( $rule, $screen, $field_group ) {
-		
+
 		// Check screen args.
-		if( isset($screen['post_type']) ) {
+		if ( isset( $screen['post_type'] ) ) {
 			$post_type = $screen['post_type'];
-		} elseif( isset($screen['post_id']) ) {
+		} elseif ( isset( $screen['post_id'] ) ) {
 			$post_type = get_post_type( $screen['post_id'] );
 		} else {
 			return false;
 		}
-		
+
 		// Page templates were extended in WordPress version 4.7 for all post types.
 		// Prevent this rule (which is scoped to the "page" post type) appearing on all post types without a template selected (default template).
-		if( $rule['value'] === 'default' && $post_type !== 'page' ) {
+		if ( $rule['value'] === 'default' && $post_type !== 'page' ) {
 			return false;
 		}
-		
-		// Match rule using Post Template logic.
-		return acf_get_location_type( 'post_template' )->match( $rule, $screen, $field_group );
+
+		// Check if this post type has templates.
+		$post_templates = acf_get_post_templates();
+		if ( ! isset( $post_templates[ $post_type ] ) ) {
+			return false;
+		}
+
+		// Get page template allowing for screen or database value.
+		if ( isset( $screen['page_template'] ) ) {
+			$page_template = $screen['page_template'];
+		} elseif ( isset( $screen['post_id'] ) ) {
+			$page_template = get_post_meta( $screen['post_id'], '_wp_page_template', true );
+		} else {
+			$page_template = '';
+		}
+
+		// Treat empty value as default template.
+		if ( $page_template === '' ) {
+			$page_template = 'default';
+		}
+
+		// Compare rule against $page_template.
+		return $this->compare_to_rule( $page_template, $rule );
 	}
-	
+
 	/**
 	 * Returns an array of possible values for this rule type.
 	 *
