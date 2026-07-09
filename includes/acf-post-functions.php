66515752abbd1092aa7e304eee3d6258fcb00129
Controlled Chaos Design <greg@ccdzine.com>
2026-07-09 06:09:32 -0700

    Code cleanup.

--- includes/acf-post-functions.php
+++ includes/acf-post-functions.php
@@ -53,71 +53,52 @@ function acf_get_post_templates() {
 }
 
 /**
- * acfe_get_post_type_objects
+ * Get post type objects
  *
- * Query & retrieve post types objects
+ * Query & retrieve post types objects.
  *
- * @param array $args
- *
+ * @since  1.0.0
+ * @param  array $args
  * @return array
  */
-function acfe_get_post_type_objects($args = array()){
+function acfe_get_post_type_objects( $args = [] ) {
 
-	// vars
-	$return = array();
+	$return      = [];
+	$posts_types = acf_get_post_types( $args );
 
-	// Post Types
-	$posts_types = acf_get_post_types($args);
-
-	// Choices
-	if(!empty($posts_types)){
-
-		foreach($posts_types as $post_type){
-
-			$post_type_object = get_post_type_object($post_type);
-
+	if ( ! empty( $posts_types ) ) {
+		foreach ( $posts_types as $post_type ) {
+			$post_type_object = get_post_type_object( $post_type );
 			$return[ $post_type_object->name ] = $post_type_object;
-
 		}
-
 	}
-
 	return $return;
-
 }
 
 /**
- * acfe_get_pretty_post_statuses
+ * Get pretty post statuses
  *
- * Similar to acf_get_pretty_post_types() but for Post Statuses
+ * Similar to acf_get_pretty_post_types() but for post status.
  *
- * @param array $posts_statuses
- *
+ * @since  1.0.0
+ * @param  array $posts_statuses
  * @return array
  */
-function acfe_get_pretty_post_statuses($posts_statuses = array()){
+function acfe_get_pretty_post_statuses( $posts_statuses = [] ) {
 
-	if(empty($posts_statuses)){
-
-		$posts_statuses = get_post_stati(array(), 'names');
-
+	if ( empty( $posts_statuses ) ) {
+		$posts_statuses = get_post_stati( [], 'names' );
 	}
+	$return = [];
 
-	$return = array();
+	if ( ! empty( $posts_statuses ) ) {
 
-	// Choices
-	if(!empty($posts_statuses)){
+		foreach ( $posts_statuses as $post_status ) {
 
-		foreach($posts_statuses as $post_status){
-
-			$post_status_object = get_post_status_object($post_status);
+			$post_status_object = get_post_status_object( $post_status );
 
 			$return[$post_status_object->name] = $post_status_object->label . ' (' . $post_status_object->name . ')';
-
 		}
-
 	}
-
 	return $return;
-
 }
